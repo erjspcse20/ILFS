fc11b71b8d2a2fa12e07b084c3cf582d2e47367a
Jai Pandey <jai.pandey@sael.co>
2019-03-27 18:08:44 +0530

    some change

--- lfs_main/logs/log-2019-03-27.php
+++ lfs_main/logs/log-2019-03-27.php
@@ -165,3 +165,5 @@ DEBUG - 2019-03-27 07:58:02 --> UTF-8 Support Enabled
 DEBUG - 2019-03-27 07:58:02 --> Global POST, GET and COOKIE data sanitized
 DEBUG - 2019-03-27 07:58:02 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
 DEBUG - 2019-03-27 12:28:02 --> Total execution time: 0.0488
+ERROR - 2019-03-27 12:31:50 --> Severity: Error --> Maximum execution time of 30000 seconds exceeded G:\software\web\il-fs\lfs_predefined\libraries\Session\drivers\Session_files_driver.php 212
+ERROR - 2019-03-27 13:34:56 --> Severity: Error --> Maximum execution time of 30000 seconds exceeded G:\software\web\il-fs\lfs_predefined\libraries\Session\drivers\Session_files_driver.php 212
